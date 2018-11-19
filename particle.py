a9c2acc839cd07d176ddc4dcd11d6fce7a328866
gbogopolsky <gbogo19@gmail.com>
2018-11-19 12:02:49 +0100

    Implementing tokamak B field and particle movement inside.

--- particle.py
+++ particle.py
@@ -6,6 +6,7 @@
 ###############################################################################
 
 import numpy as np
+import constants as cst
 
 # We define the particle class
 class Particle:
@@ -44,3 +45,28 @@ class Particle:
         
         # Updating position
         self.r += dt * self.v
+        
+        
+class Tokamak:
+    def __init__(self, R, r, n, N, I):
+        self.R = R
+        self.r = r
+        self.n = n
+        self.N = N        
+        self.I = I
+        
+    def getB(self,x,y,z):
+        r_x = x - self.R*x/np.sqrt(x**2+y**2)
+        r_y = y - self.R*y/np.sqrt(x**2+y**2)
+        r_z = z
+        if (np.sqrt(x**2 + y**2) < (self.R-self.r)) or (np.sqrt(x**2 + y**2) > (self.R+self.r)):
+            B = np.array((0, 0, 0), dtype='float64')
+        else:
+            if np.sqrt(r_x**2 + r_y**2 + r_z**2) > self.r:
+                B = np.array((0, 0, 0), dtype='float64')
+            else:
+                B = np.array((0, 0, 0), dtype='float64')
+                B[0] = - ((cst.mu0*self.n*self.N*self.I) / (2*np.pi*(x**2+y**2)))*y
+                B[1] = ((cst.mu0*self.n*self.N*self.I) / (2*np.pi*(x**2+y**2)))*x
+                B[2] = 0
+        return(B)
---
+++ tokamak.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#                           Test of magnetic field
+#
+###############################################################################
+
+import numpy as np
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import constants as cst
+import particle as part
+plt.rcParams['legend.fontsize'] = 18
+plt.rcParams['figure.figsize'] = (6,6)
+
+#N = 50
+#z = 0
+#x, y = np.arange(N)+1, np.arange(N)+1
+#Bx, By, Bz = np.zeros((N,N)), np.zeros((N,N)),  np.zeros((N,N)) 
+
+tok = part.Tokamak(4,2,1000,20,10000)
+#for i in range(N):
+#    Bx[i], By[i], Bz[i] = tok.getB(x[i], y[i], z)
+
+#plt.contourf(x, y, Bx, 64)
+#plt.colorbar()
+#plt.show()
+#plt.contourf(x, y, By, 64)
+#plt.colorbar()
+#plt.show()
+
+N = 1       # Number of particles
+
+# Parameters and fields
+charge = cst.elemCharge
+mass = cst.Mp
+E0 = np.array((0, 0, 0))
+B0 = np.array((0, 0, 1))
+w0 = np.abs(charge) * np.sqrt(np.sum(B0*B0, axis=0)) / mass
+dt = 0.01 / w0       # timestep
+Np = 50             # Number of cyclotronic periods
+
+Tf = Np * 2 * np.pi / w0
+Nt = int(Tf // dt)  # Number of timesteps
+t = np.arange(0, Nt)*dt
+x, y, z = np.zeros((Nt)), np.zeros((Nt)), np.zeros((Nt))    # positions taken by the particle along 
+vx, vy, vz = np.zeros((Nt)), np.zeros((Nt)), np.zeros((Nt))   # velocities
+
+part1 = part.Particle(mass, charge)
+part1.initPos(tok.R, 0, 0)
+part1.initSpeed(0, 100, 1)
+
+for i in range(Nt):
+    E = E0
+    B = tok.getB(*part1.r)
+    part1.push(dt, E, B)
+    x[i], y[i], z[i] = part1.r
+    vx[i], vy[i], vz[i] = part1.v
+    
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.plot(x, y, z)
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+plt.show()
+plt.plot(t,E)
+plt.xlim((t[0], t[-1]))
+plt.show()
