31d560c133c5ab76f8b7480e587544aeba9dcc8b
Guillaume <gbogo19@gmail.com>
2018-12-16 22:05:44 +0100

    First test case and update to plots.

--- main.py
+++ main.py
@@ -11,23 +11,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import constants as cst
 from particle import Particle
-mpl.rcParams['legend.fontsize'] = 10
+plt.rcParams["figure.figsize"] = (9,8)
+plt.rcParams['font.size'] = 18
 
 N = 1       # Number of particles
 
 # Parameters and fields
 charge = cst.elemCharge
 mass = cst.Mp
-E0 = np.array((0, 1, 0))
+E0 = np.array((0, 0, 0))
 B0 = np.array((0, 0, 1))
 w0 = np.abs(charge) * np.sqrt(np.sum(B0*B0, axis=0)) / mass
-dt = 0.01 / w0       # timestep
+dt = 0.001 / w0       # timestep
 Np = 10             # Number of cyclotronic periods
 
 Tf = Np * 2 * np.pi / w0
 Nt = int(Tf // dt)  # Number of timesteps
 t = np.arange(0, Nt)*dt
-x, y, z = np.zeros((Nt)), np.zeros((Nt)), np.zeros((Nt))    # positions taken by the particle along 
+x, y, z = np.zeros((Nt)), np.zeros((Nt)), np.zeros((Nt))    # positions taken by the particle along
 vx, vy, vz = np.zeros((Nt)), np.zeros((Nt)), np.zeros((Nt))   # velocities
 
 part = Particle(mass, charge)
@@ -35,50 +36,59 @@ part.initPos(0, 0, 0)
 part.initSpeed(200, 0, 0)
 
 for i in range(Nt):
-    E = E0*np.cos(0.01*w0*t[i])
-    B = B0*np.cos(0.01*w0*t[i])
-    part.push(dt, E, B)
+    part.push(dt, E0, B0)
     x[i], y[i], z[i] = part.r
     vx[i], vy[i], vz[i] = part.v
-    
+
 # Diagnostics
 E = 0.5 * mass * (vx**2 + vy**2 + vz**2)
 vx_th = 200 * np.cos(w0*t)
 vx_error = np.abs(vx - vx_th)
 print(np.max(vx_error))
-vz_th = 200 * np.sin(w0*t)
-vz_error = np.abs(vz - vz_th)
-print(np.max(vx_error))
+vy_th = 200 * np.sin(w0*t + np.pi)
+vy_error = np.abs(vy - vy_th)
+print(np.max(vy_error))
 
 # Outputs
+## Speed along x
 plt.plot(t, vx, label='numeric')
-#plt.plot(t, vx_th, label='analytic')
-#plt.plot(t, vx_error, label='erreur absolue')
+plt.plot(t, vx_th, label='analytic')
+plt.plot(t, vx_error, label='erreur absolue')
+plt.xlabel('$t$ [s]')
+plt.ylabel('$v_x$ [m/s]')
 plt.legend()
 plt.xlim((t[0], t[-1]))
+plt.tight_layout()
+plt.savefig('test_vx.png')
 plt.show()
 
-plt.plot(t, vz, label='numeric')
-#plt.plot(t, vz_th, label='analytic')
-#plt.plot(t, vz_error, label='erreur absolue')
+## Speed along y
+plt.plot(t, vy, label='numeric')
+plt.plot(t, vy_th, label='analytic')
+plt.plot(t, vy_error, label='erreur absolue')
+plt.xlabel('$t$ [s]')
+plt.ylabel('$v_y$ [m/s]')
 plt.legend()
 plt.xlim((t[0], t[-1]))
+plt.tight_layout()
+plt.savefig('test_vy.png')
 plt.show()
 
+## 3D trajectory
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-ax.plot(x, y, z)
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
+ax.plot(x*1e6, y*1e6, z)
+ax.set_xlabel('$x$ [µm]')
+ax.set_ylabel('$y$ [µm]')
+ax.set_zlabel('$z$ [m]')
+plt.savefig('test_3d.png')
 plt.show()
-plt.plot(t,E)
+
+## Energy vs. time
+plt.plot(t, E)
+plt.xlabel('$t$ [s]')
+plt.ylabel('$E_c$ [J]')
 plt.xlim((t[0], t[-1]))
+plt.tight_layout()
+plt.savefig('test_E.png')
 plt.show()
-    
-    
-
-
-
-
-
